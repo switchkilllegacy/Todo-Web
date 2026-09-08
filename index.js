810b26ed98912c7a307c21657c2d4957621308c0
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-08 11:42:38 +0300

    Write data to file

--- index.js
+++ index.js
@@ -7,16 +7,46 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    // get data from file
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // tasks list data from file
+      const tasks = data.split('\n');
+      resolve(tasks);
+    });
+  });
+};
+
 app.get('/', (req, res) => {
-  // get data from file
-  fs.readFile('./tasks', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    // tasks list data from file
-    const tasks = data.split('\n');
+  // tasks list data from file
+  readFile('./tasks').then((tasks) => {
+    console.log(tasks);
     res.render('index', { tasks: tasks });
+  });
+});
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  // tasks list data from file
+  readFile('./tasks').then((tasks) => {
+    // add form sent task to tasks array
+    tasks.push(req.body.task);
+    const data = tasks.join('\n');
+    fs.writeFile('./tasks', data, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // redirect to / to see result
+      res.redirect('/');
+    });
   });
 });
 
