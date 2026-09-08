94ca477fba6a00f3a8be02d29e1cda44cb3cb5f9
switchkilllegacy <mareka.tallinski@gmail.com>
2026-09-08 11:06:07 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 // use ejs files to prepare for views
 const path = require('path');
@@ -7,7 +8,16 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  // get data from file
+  fs.readFile('./tasks', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // tasks list data from file
+    const tasks = data.split('\n');
+    res.render('index', { tasks: tasks });
+  });
 });
 
 app.listen(3001, () => {
